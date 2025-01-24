ea63db4f1db5be48aae1b49b5f2badfd2ee7c0b8
megalamanokaran26@gmail.com <megalamanokaran26@gmail.com>
2025-01-24 21:58:58 +0530

    vehicles1

---
+++ vehicles1.java
@@ -0,0 +1,40 @@
+class vehicle
+{
+String brand;
+void brandname(String brand)
+{
+this.brand=brand;
+}
+void display()
+{
+System.out.println("vehicle brand:"+brand);
+}
+}
+class car extends vehicle
+{
+void drive()
+{
+System.out.println("this car very expensive:");
+}
+}
+class bike extends vehicle
+{
+void ride()
+{
+System.out.println("this is my bike");
+}
+}
+public class vehicles1
+{
+public static void main(String args[])
+{
+car car=new car();
+car.brandname("BMW");
+car.display();
+car.drive();
+bike bike=new bike();
+bike.brandname("royal enfield");
+bike.display();
+bike.ride();
+}
+}
